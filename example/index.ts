f80265ae8544f9971629b974406900c8c191e460
Stephan Leroux <stephan.leroux@shopify.com>
2020-01-31 12:01:38 -0500

    Added shopify_foyer exr file, background and envMap to cube material

--- example/index.ts
+++ example/index.ts
@@ -6,12 +6,29 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
+  FloatType,
+  Texture,
+  WebGLRenderTarget,
+  MeshStandardMaterial,
+  Mesh,
 } from 'three';
+import {
+  EXRLoader,
+} from 'three/examples/jsm/loaders/EXRLoader';
+import {
+  PMREMGenerator,
+} from 'three/src/extras/PMREMGenerator';
 import threeGltfVariantLoader from '../src';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const renderer = new WebGLRenderer({canvas});
+
+const pmremGenerator = new PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
 let currentVariantScene: Scene;
+let exrCubeRenderTarget: WebGLRenderTarget;
+let exrBackground: Texture;
 
 renderer.setSize(canvas.offsetWidth, canvas.offsetHeight, false);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -34,6 +51,16 @@ camera.position.z = 5;
 scene.add(new AmbientLight(0x404040, 1.5));
 const light = new DirectionalLight();
 scene.add(light);
+
+new EXRLoader()
+  .setDataType(FloatType)
+  .load('assets/shopify_foyer.exr',
+  (texture: Texture) => {
+    exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture);
+    exrBackground = exrCubeRenderTarget.texture;
+    texture.dispose();
+  }
+);
 
 threeGltfVariantLoader('./assets/variant.glb', (variantSwitcher, gltf) => {
   function switchTag(tag: string) {
@@ -72,6 +99,15 @@ function render() {
     currentVariantScene.rotation.x += 0.01;
     currentVariantScene.rotation.y += 0.01;
   }
+
+  if (currentVariantScene && exrCubeRenderTarget) {
+    const root = (currentVariantScene.children[0] as Mesh);
+    const material = (root.material as MeshStandardMaterial);
+    material.envMap = exrCubeRenderTarget.texture;
+    material.needsUpdate = true;
+  }
+
+  scene.background = exrBackground;
 
   renderer.setClearColor(0xffffff);
   renderer.clearColor();
